76ef41b7f4927ac69d1b4f76d732e477e31ade34
Harshit-Sharma2004 <sharmaharshit3310@gmail.com>
2025-07-09 09:58:57 +0530

    Create assignment_08.py

---
+++ assignment_08.py
@@ -0,0 +1,6 @@
+#QUES-1 :- Create a Numpy array with random values (4x2) empty and a full NumPy array (3x5) array filled with all zeros (4x3x2) array filled with all ones
+
+import numpy as np
+
+a=np.zeros((3,5),'i4')
+print(a)
